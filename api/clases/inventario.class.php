9e3fe7097605c04681d0a1ce6fc73d41264426af
ezequielgmart <ezequielgm677@gmail.com>
2021-01-13 13:39:06 -0400

    13/01/2020 - inventario - DELETE listo

--- api/clases/inventario.class.php
+++ api/clases/inventario.class.php
@@ -211,8 +211,69 @@
             }
         
     }
+   
     public function delete($headers,$json){
-        
+        $_auth = new auth();
+        $_respuestas = new respuestas();
+
+        # guardamos el token
+        $auth =  $headers["auth"];
+
+        $body = json_decode($json,true);
+
+        # Validamos que el token este READY TO GO
+        $verificar = $_auth->validar_token($auth);
+
+        if ($verificar == 0) {
+
+                echo json_encode($_respuestas->code_401("Token invalido"));
+
+        } else {
+                    
+            // Este metodo retorna el id del usuario recibiendo por parametro un token ya verificado
+            $this->id_usuario = parent::id_usuario($verificar);
+
+            if ($this->id_usuario == 0) {
+                
+                echo json_encode($_respuestas->code_401("Token invalido o no se encuentra en la peticion"));
+
+            } else {
+                
+                if (is_numeric($body["id_movimiento"])) {
+
+                    $this->id_movimiento = $body["id_movimiento"];
+
+                    $sql = "DELETE  
+                    FROM ".$this->table."
+                    WHERE 
+                    id_movimiento='".$this->id_movimiento."' 
+                    AND
+                    id_usuario ='".$this->id_usuario."'";
+
+                    $consultar = parent::modificar_bdd($sql);
+
+
+                    if ($consultar > 0) {
+
+                        http_response_code(200);
+                        echo json_encode($_respuestas->code_200("Movimiento de inventario eliminado correctamente"));
+
+                    } else {
+
+                        http_response_code(500);
+                        echo json_encode($_respuestas->code_500("no se pudo eliminar el movimiento de inventario"));
+
+                    }
+
+                }else{
+                      
+                    echo json_encode($_respuestas->code_400("El id_asiento debe ser un numero"));
+                                        
+                }
+                                     
+            }
+        }
+    
     }
 
 
